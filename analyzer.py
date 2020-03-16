ee862d6e06081061df4998811167a320606cce8f
Harsh188 <hiharshith18@gmail.com>
2020-03-16 18:04:59 +0530

    initial commit

---
+++ analyzer.py
@@ -0,0 +1,9 @@
+"""
+Authors: Harshith MohanKumar & Swarupa
+Date: 16/3/2020
+analyzer.py
+Description: This program aims to build a sentiment analyzer which can predict the mood
+of a given text. It will classify the text into neutral, positive or negative.
+"""
+
+
